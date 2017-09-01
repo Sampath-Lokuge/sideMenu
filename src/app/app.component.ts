0ac2a246abe81e20af2c14fe4167237d944fdbc1
Sampath-Lokuge <sugeesampathlokuge@gmail.com>
2017-09-01 16:17:44 +0530

    2nd push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,18 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any, icon: string }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage, icon: 'game-controller-b' },
+      { title: 'Activity', component: '', icon: 'browsers' },
+      { title: 'Contacts', component: '', icon: 'play' },
+      { title: 'Add Project', component: '', icon: 'settings' },
+      { title: 'Settings', component: '', icon: 'settings' }
     ];
 
   }
